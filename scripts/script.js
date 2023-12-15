00e53e0f98bd49bf04b43223afb9d389f7cb8770
gal aspis <galaspis@gmail.com>
2023-12-15 12:50:53 +0200

    bug

--- scripts/script.js
+++ scripts/script.js
@@ -66,9 +66,8 @@ function setName() {
 
     var selectedName = document.getElementById('nameSelect').value;
     console.log('Selected name:', selectedName);  // This should log the selected option's value
+
     
-    var nameButton = document.querySelector('.nav-button');
-    console.log('Name button element:', nameButton);  // This should log the element or null
 
     // Check if the elements are correctly selected
     if (selectedName && nameButton) {
